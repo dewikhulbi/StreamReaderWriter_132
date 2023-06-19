c1de387b0c735f5917c475e8530a412ab4323bee
dewikhulbi <dewikhlb20@gmail.com>
2023-06-19 11:00:21 +0700

    untuk menutup file

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -20,5 +20,13 @@ int main() {
 	//unlimited loop untuk menulis
 	while (true) {
 		cout << "_ ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q")break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
 	}
+	//selesaikan dalam menulis sekarang tutup filenya
+	outfile.close();
 }
